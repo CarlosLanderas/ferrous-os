091cca338ccb63eacab5a71dcc7bbc25d723b306
Carlos Landeras <carlos.landeras@gmail.com>
2021-05-17 07:21:37 +0200

    Added testable trait

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,10 @@ fn panic(info: &PanicInfo) -> ! {
 
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -62,7 +62,18 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_println!("trivial assertion... ");
     assert_eq!(1,1);
-    serial_println!("[ok]");
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where T: Fn(), {
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
 }
