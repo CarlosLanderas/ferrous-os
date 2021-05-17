ae7225170844cb524e111c638ab7155e0cfdde49
Carlos Landeras <carlos.landeras@gmail.com>
2021-05-17 07:54:52 +0200

    println test on boot

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,15 +5,21 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use oxid_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     test_main();
-    
+
     loop {}
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     oxid_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
