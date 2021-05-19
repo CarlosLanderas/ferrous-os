26868f922cf5349e56f9a62e316cf407c6b488dc
Carlos Landeras <carlos.landeras@gmail.com>
2021-05-19 10:09:26 +0200

    Implement boot info frame allocator

--- src/main.rs
+++ src/main.rs
@@ -6,34 +6,29 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use oxid_os::{println};
-use x86_64::{VirtAddr, structures::paging::Translate};
+use oxid_os::{memory, println};
 
 entry_point!(kernel_main);
 
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    
+    use oxid_os::memory;
+    use x86_64::{structures::paging::Page, VirtAddr}; 
     println!("Hello World{}", "!");
     
     oxid_os::init();
 
     println!("Physical memory offset: 0x{:x}", boot_info.physical_memory_offset);
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { oxid_os::memory::init(phys_mem_offset)};
-    let addresses = [        
-        0xb8000,        
-        0x201008,        
-        0x0100_0020_1a10,        
-        boot_info.physical_memory_offset,
-    ];
+    let mut mapper = unsafe { memory::init(phys_mem_offset)};
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
+    //map an unused page
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)]
     test_main();
